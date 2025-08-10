4062dc92241fed64f27ad01c61243cbfc3aab129
Bruce <bruce@redravencomputing.com>
2025-08-10 15:08:35 -0500

    Initial Commit

---
+++ llamaTest/llamaTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  llamaTestApp.swift
+//  llamaTest
+//
+//  Created by Bruce Burgess on 8/10/25.
+//
+
+import SwiftUI
+
+@main
+struct llamaTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
